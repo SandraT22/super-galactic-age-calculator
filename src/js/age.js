cd568d2c8d437d7afa44dac2de194437f91f0a18
Sandra Tena <sandratena06@gmail.com>
2022-06-24 09:51:45 -0700

    refactor business logic for age prototype

--- src/js/age.js
+++ src/js/age.js
@@ -4,31 +4,50 @@ export default class Age {
     this.planet = planet;
   }
 
-  mercuryAge() {
+  planetaryAge() {
     if (this.planet === "Mercury") {
       let mercuryAge = this.age/.24
       return parseInt(mercuryAge);
-    }
-  }
-
-  venusAge() {
-    if (this.planet === "Venus") {
+    } else  if (this.planet === "Venus") {
       let venusAge = this.age/.62
       return parseInt(venusAge);
-    }
-  }
-
-  marsAge() {
-    if (this.planet === "Mars") {
+    } else if (this.planet === "Mars") {
       let marsAge = this.age/1.88
       return parseInt(marsAge);
-    }
-  } 
-
-  jupiterAge() {
-    if (this.planet === "Jupiter") {
+    } else if (this.planet === "Jupiter") {
       let jupiterAge = this.age/11.86
       return parseInt(jupiterAge);
+    } else {
+      return "Please choose a planet"
     }
-  } 
-}
+  }
+};
+
+  // mercuryAge() {
+  //   if (this.planet === "Mercury") {
+  //     let mercuryAge = this.age/.24
+  //     return parseInt(mercuryAge);
+  //   }
+  // }
+
+  // venusAge() {
+  //   if (this.planet === "Venus") {
+  //     let venusAge = this.age/.62
+  //     return parseInt(venusAge);
+  //   }
+  // }
+
+  // marsAge() {
+  //   if (this.planet === "Mars") {
+  //     let marsAge = this.age/1.88
+  //     return parseInt(marsAge);
+  //   }
+  // } 
+
+//   jupiterAge() {
+//     if (this.planet === "Jupiter") {
+//       let jupiterAge = this.age/11.86
+//       return parseInt(jupiterAge);
+//     }
+//   } 
+// }
