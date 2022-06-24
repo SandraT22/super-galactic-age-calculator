995e8335a83e18f61d3600d529c17fe19e1ea768
Sandra Tena <sandratena06@gmail.com>
2022-06-24 16:42:09 -0700

    rewrite planetary life left

--- src/js/yearsLeft.js
+++ src/js/yearsLeft.js
@@ -4,11 +4,7 @@ export default class YearsLeft {
     this.planetaryExpectancy = planetaryExpectancy;
   }
 planetaryLifeLeft() {
-  let lifeLeft = 0;
-  let age = new Age();
-  let planetaryAge = age.planetaryAge();
-  if (this.agePlanet === "Mercury"){
-    return planetaryAge;
-  }
+  let yearsLeft = this.planetaryAge - this.planetaryExpectancy;
+  return yearsLeft;
 }
 }
