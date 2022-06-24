51da523c60ba4430ca0364045e21f496c3ab8aa1
Sandra Tena <sandratena06@gmail.com>
2022-06-24 11:43:36 -0700

    add test 3

--- __tests__/lifeExpectancy.test.js
+++ __tests__/lifeExpectancy.test.js
@@ -2,9 +2,13 @@ import LifeExpectancy from './../src/js/lifeExpectancy.js';
 
 describe("LifeExpectancy", () => {
   let expectancy1;
+  let expectancy2;
+  let expectancy3;
 
   beforeEach(() => {
     expectancy1 = new LifeExpectancy("Small Town");
+    expectancy2 = new LifeExpectancy("Rural");
+    expectancy3 = new LifeExpectancy("City");
   })
 
   test("should check that constructor works properly", () => {
@@ -13,5 +17,13 @@ describe("LifeExpectancy", () => {
 
   test("should check that lifestyle choices alter the life expectancy", () => {
     expect(expectancy1.expectancyLifestyle()).toEqual(100);   
+  });
+
+  test("should check that lifestyle choices alter the life expectancy", () => {
+    expect(expectancy2.expectancyLifestyle()).toEqual(95);   
+  });
+
+  test("should check that lifestyle choices alter the life expectancy", () => {
+    expect(expectancy3.expectancyLifestyle()).toEqual(90);   
   });
 })
