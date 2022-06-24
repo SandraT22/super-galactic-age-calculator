9f24064107b950e2347500ef65a18e0f74c056b7
Sandra Tena <sandratena06@gmail.com>
2022-06-24 11:05:47 -0700

    add business logic for test 1-life expectancy

--- src/js/lifeExpectancy.js
+++ src/js/lifeExpectancy.js
@@ -4,4 +4,17 @@ export default class LifeExpectancy {
     this.activityLevel = activityLevel;
     this.eatingHabits = eatingHabits;
   }
+  lifestyle () {
+    let expectancyLifestyle = 100;
+    let smallTown = 0;
+    let rural = 5;
+    let city = 10;
+    if (this.lifestyle === "Small Town") {
+      return expectancyLifestyle + smallTown;
+    } else if (this.lifestyle === "Rural") {
+      return expectancyLifestyle - rural;
+    } else if (this.lifestyle === "City") {
+      return expectancyLifestyle - city;
+    }
+  }
 }
