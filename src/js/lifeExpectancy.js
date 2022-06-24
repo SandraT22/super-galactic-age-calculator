baf15d434f6081d7b0628fe70046cc7ad4524436
Sandra Tena <sandratena06@gmail.com>
2022-06-24 10:55:25 -0700

    add business logic to lifeExpectancy.js

---
+++ src/js/lifeExpectancy.js
@@ -0,0 +1,7 @@
+export default class LifeExpectancy {
+  constructor(lifestyle, activityLevel, eatingHabits) {
+    this.lifestyle = lifestyle;
+    this.activityLevel = activityLevel;
+    this.eatingHabits = eatingHabits;
+  }
+}
